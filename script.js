ae1c6a5d56743ef0d92b7357634684d994ad9a95
didprojects <cassieliu_nz@163.com>
2023-04-19 18:02:12 +1200

    use forEach

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
 const cntNum = document.querySelector(".counter-number");
-const decreaseBtn = document.querySelector(".decrease-btn");
-const resetBtn = document.querySelector(".reset-btn");
-const increaseBtn = document.querySelector(".increase-btn");
+const btns = document.querySelectorAll(".btn");
 
-resetBtn.addEventListener("click", function(){
-    cntNum.textContent = 0;
+let cnt = 0;
+btns.forEach(function(btn){
+    btn.addEventListener("click",function(e){
+        const styles = e.currentTarget.classList;
+        if(styles.contains("decrease")){
+            cnt--;
+        }else if(styles.contains("increase")){
+            cnt++;
+        }else{
+            cnt = 0;
+        }
+        if(cnt > 0){
+            cntNum.style.color = "green";
+        }else if(cnt < 0){
+            cntNum.style.color = "red";
+        }else{
+            cntNum.style.color = "black";
+        }
+        cntNum.textContent = cnt;
+    });
 })
 
-decreaseBtn.addEventListener("click", function(){
-    let curNum = cntNum.innerHTML;
-    cntNum.textContent = parseInt(curNum)-1;
-})
 
-increaseBtn.addEventListener("click", function(){
-    let curNum = cntNum.textContent;
-    cntNum.textContent = parseInt(curNum)+1;
-})
+
+
+
+// const decreaseBtn = document.querySelector(".decrease-btn");
+// const resetBtn = document.querySelector(".reset-btn");
+// const increaseBtn = document.querySelector(".increase-btn");
+
+// resetBtn.addEventListener("click", function(){
+//     cntNum.textContent = 0;
+// })
+
+// decreaseBtn.addEventListener("click", function(){
+//     let curNum = cntNum.innerHTML;
+//     cntNum.textContent = parseInt(curNum)-1;
+// })
+
+// increaseBtn.addEventListener("click", function(){
+//     let curNum = cntNum.textContent;
+//     cntNum.textContent = parseInt(curNum)+1;
+// })
